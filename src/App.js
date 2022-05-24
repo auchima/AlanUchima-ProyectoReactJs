bd5ff36f76d8b70e7703b80ba4f9576c7fb7f732
auchima <alan.uchima@gmail.com>
2022-05-24 16:12:34 -0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import './App.scss';
-import {Navbar as Bnavbar, Container, Nav, NavDropdown} from "react-bootstrap";
-import {Navbar} from "./Navbar"
+import NavBar from './component/NavBar/NavBar';
 
 function App() {
   return (
   <div>
-      <Navbar></Navbar>
-  <Bnavbar bg="light" expand="lg">
-            <Container>
-                <Bnavbar.Brand href="#home">Uchima Wines</Bnavbar.Brand>
-                <Bnavbar.Toggle aria-controls="basic-navbar-nav" />
-                <Bnavbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                </Bnavbar.Collapse>
-            </Container>
-        </Bnavbar>
-
+    <NavBar/>
   </div>
   );
 }
