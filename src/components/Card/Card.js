6c810f51edf7598274cb0530e7082263617a7be4
auchima <alan.uchima@gmail.com>
2022-05-24 21:21:58 -0300

    agrego contador

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,20 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { Button } from '@mui/material';
+import {useState} from 'react';
 
 const CardItem = () =>{
+
+    const [count, setCount] = useState(0);
+
+    const addCount = () =>{
+        setCount(count + 1)
+    }
+
+    const removeCount = () =>{
+        setCount (count - 1)
+    }
+
     return(
         <Card sx={{ minWidth: 275 }}>
             <CardContent>
@@ -12,7 +24,12 @@ const CardItem = () =>{
                     </div>
                     <p>Vino Tinto</p>
                     <span>$ 450</span>
-                    <Button>Detalle</Button>
+                    <div className = 'count-item'>
+                        <Button onClick={removeCount}>-</Button>
+                        <p>{count}</p>
+                        <Button onClick={addCount}>+</Button>
+                    </div>
+                    <Button>Agregar</Button>
                 </div>
             </CardContent>
             </Card>
