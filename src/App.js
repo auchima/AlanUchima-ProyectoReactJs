219dab9449f617952461f60ac1e3abca37949a29
auchima <alan.uchima@gmail.com>
2022-05-24 21:02:19 -0300

    Landing Completo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 import CardItem from './components/Card/Card';
+import { Container, Grid } from '@mui/material';
 
 
 function App() {
   return (
   <div>
     <NavBar/>
-    <CardItem/>
+    <Container>
+      <Grid container>
+        <Grid item md={4}>
+        <CardItem/>
+        </Grid>
+      </Grid>
+    </Container>
   </div>
   );
 }
